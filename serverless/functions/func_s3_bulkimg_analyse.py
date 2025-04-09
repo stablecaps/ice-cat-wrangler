401741a0dcb148fd83981e44f2ea8ba6e51fe115
Darkpandarts <14529342+darkpandarts@users.noreply.github.com>
2025-04-09 20:01:35 +0100

    lint func_s3_bulkimg_analyse.py

--- serverless/functions/func_s3_bulkimg_analyse.py
+++ serverless/functions/func_s3_bulkimg_analyse.py
@@ -87,38 +87,7 @@ def check_bucket_exists(bucket_name):
 
 def run(event, context):
     """
-    Main lambda entrypoint & logic.
-
-    user -> uploads image to s3bucketSource -> this.lambda
-        1. submits image to rekognition (write DynDB record)
-        2. gets rekognition response (update DynDB)
-        3. success -> moves image to s3bucketDest (update DynDB)
-        4. failure -> moves image to s3bucketFail (update DynDB)
-
-    Additional notes:
-        1. Save hash of file to avoid reprocessing
-        2. uniquely rename file to prevent filename clashes in s3 (use hash)
-
-    DDB use cases (can be single or batch):
-        1. track image processing status
-        2. track rekognition response (is a cat?)
-        3. track image location/filename (source, dest, fail)
-        4. track image hash (to avoid reprocessing) (is the main key)
-        5. track logs associated with each submission
-
-        * So retrive a single entry
-        * Or retrive a batch of entries
-
-    DynamoDB entries:
-        1. [str] image hash
-        2. [int] upload batch id - to group images batch uploaded (for batch retrieval)
-        3. [str] image filename in bucket location (will be updated with any renaming process)
-        4. [str] operation status (pending, success, fail)
-        5. [json] rekognition response as json
-        6. [bool] rekognition: is image a cat?
-        7. [json] logs for debug retrieval?
-        8. [datetime] upload timestamp
-        9. [datetime] rekognition timestamp
+    Main lambda entrypoint & logic
 
     """
 
