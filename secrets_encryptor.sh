a054663fee865755239bd022468ebd7202882c56
Darkpandarts <14529342+darkpandarts@users.noreply.github.com>
2025-04-12 19:46:36 +0100

    update secrets en/decryptor and reupload secrets

--- secrets_encryptor.sh
+++ secrets_encryptor.sh
@@ -2,10 +2,12 @@
 
 set -eo pipefail
 
-echo -e "Usage:\n"
-echo -e "1. Provide a file path containing the password: $0 /path/to/password_file"
-echo -e "2. Provide a text password directly: $0 '\$password'\n"
-
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo -e "Usage:\n"
+  echo -e "1. Provide a file path containing the password: $0 /path/to/password_file"
+  echo -e "2. Provide a text password directly: $0 '\$password'\n"
+  exit 0
+fi
 
 if ! command -v openssl &>/dev/null ; then
   echo "openssl is not installed. Please install it and try again."
